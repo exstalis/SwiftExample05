dc99ecd151faf483e999702076d17bb8aaae5cfb
elif ece arslan <exstalis@gmail.com>
2015-11-20 19:35:22 -0500

    Initial Commit

---
+++ SwiftExample05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExample05
+//
+//  Created by elif ece arslan on 11/20/15.
+//  Copyright © 2015 elif ece arslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
